ad7dbe963a7d73244b14ac346afc4fe2839c1d07
Santiago Pardal <santiagopardal@outlook.com>
2022-07-17 13:48:21 -0300

    indentation

--- camerapp/src/components/CameraVideos.vue
+++ camerapp/src/components/CameraVideos.vue
@@ -1,35 +1,35 @@
 <template>
-  <div>
-    <h3>Videos from {{ camera_name }}:</h3>
-    <ul v-if="videos">
-      <li v-for="video in videos" :key="video">
-        <a :href="download_url(video.day)">{{ video.day }}</a>
-      </li>
-    </ul>
-  </div>
+    <div>
+        <h3>Videos from {{ camera_name }}:</h3>
+        <ul v-if="videos">
+            <li v-for="video in videos" :key="video">
+                <a :href="download_url(video.day)">{{ video.day }}</a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import axios from 'axios'
-import { BASE_URL } from '@/constants.js'
+    import axios from 'axios'
+    import { BASE_URL } from '@/constants.js'
 
-export default {
-  name: 'CameraVideos',
-  props: {
-    camera_name: { type: String, required: true },
-    camera_id: { type: Number, required: true }
-  },
-  data() {
-    return { videos: null }
-  },
-  created() {
-    axios.get(`${BASE_URL}/cameras/${this.camera_id}/videos/`)
-        .then((response) => this.videos = response.data)
-        .catch((error) => console.log(error))
-  },
-  methods: {
-    download_url(date) {
-      return `${BASE_URL}/cameras/${this.camera_id}/videos/download/${date}` }
-  }
-}
+    export default {
+        name: 'CameraVideos',
+        props: {
+        camera_name: { type: String, required: true },
+        camera_id: { type: Number, required: true }
+    },
+    data() {
+        return { videos: null }
+    },
+    created() {
+        axios.get(`${BASE_URL}/cameras/${this.camera_id}/videos/`)
+            .then((response) => this.videos = response.data)
+            .catch((error) => console.log(error))
+    },
+    methods: {
+        download_url(date) {
+            return `${BASE_URL}/cameras/${this.camera_id}/videos/download/${date}` }
+        }
+    }
 </script>
--- camerapp/src/components/TheVideo.vue
+++ camerapp/src/components/TheVideo.vue
@@ -1,16 +1,15 @@
 <template>
-  <div>
-    <video width="1000" controls>
-      <source src="http://localhost:8080/api/cameras/2/videos/stream/17-07-2022" type="video/mp4">
-    </video>
-  </div>
+    <div>
+        <video width="1000" controls>
+            <source src="http://localhost:8080/api/cameras/2/videos/stream/17-07-2022" type="video/mp4">
+        </video>
+    </div>
 </template>
 
 <script>
-
-export default {
-  name: "TheVideo",
-}
+    export default {
+        name: "TheVideo"
+    }
 </script>
 
 <style scoped>
