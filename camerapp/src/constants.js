a600ddfdffe9aa546bfc31a9ad16a6d849a23e72
Santiago Pardal <santiagopardal@outlook.com>
2023-01-07 12:45:25 -0300

    start over

--- camerapp/src/constants.js
+++
@@ -1,3 +0,0 @@
-const BASE_URL = 'http://localhost:8080/api'
-
-module.exports = { BASE_URL }
--- camerapp/src/router/index.js
+++
@@ -1,9 +0,0 @@
-import Main from "@/views/MainView";
-import CameraStatisticsView from "@/views/CameraStatisticsView";
-
-const routes = [
-    { path: '/', component: Main },
-    { path: '/camera/:camera', component: CameraStatisticsView }
-]
-
-export default routes
