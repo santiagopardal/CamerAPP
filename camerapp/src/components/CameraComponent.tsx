68a1c11e47eccdbebc40c046996c6924e7af4253
Santiago Pardal <santiagopardal@outlook.com>
2023-01-07 17:03:16 -0300

    style

--- camerapp/src/components/CameraComponent.tsx
+++ camerapp/src/components/CameraComponent.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Camera from '../models/Camera'
+import './CameraComponent.css'
 
 function CameraComponent(properties: {camera: Camera}) {
     let camera: Camera = properties.camera
     return (
-        <>
+        <div className='camera'>
             <h1>{camera.getName()}</h1>
-            <a href={camera.getURL()}>Go to camera</a>
-        </>
+            <div className='cameraBottom'>
+                <p className='cameraBottomItem'>Is online</p>
+                <a href={camera.getURL()} className='cameraBottomItem'>Go to camera</a>
+            </div>
+        </div>
     )
 }
 
