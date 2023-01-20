c0069ea1c60682555a710a07dae5da13dc544d32
Santiago Pardal <santiagopardal@outlook.com>
2023-01-19 21:23:07 -0300

    collapsible menu

--- camerapp/src/App.tsx
+++ camerapp/src/App.tsx
@@ -1,15 +1,53 @@
-import { useState } from 'react'
-import NavBar from './CamerAINavBar'
-import CamerAIRoutes from "./Routes";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState} from 'react'
+import CamerAIRoutes from './Routes'
+import {NodeIndexOutlined, HomeOutlined, VideoCameraOutlined,} from '@ant-design/icons'
+import { Layout, Menu, theme } from 'antd'
+import './index.css'
+import {ItemType} from "antd/es/menu/hooks/useItems";
+import {useNavigate} from "react-router-dom";
+
+const { Sider, Content } = Layout;
 
 function App() {
-  return (
-      <>
-          <NavBar/>
-          <CamerAIRoutes/>
-      </>
-  )
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken()
+
+    const navigate = useNavigate()
+
+    const menuOptions: ItemType[] = [
+        {
+            key: '1',
+            icon: <HomeOutlined />,
+            label: 'Home',
+            onClick: () => navigate('')
+        },
+        {
+            key: '2',
+            icon: <VideoCameraOutlined />,
+            label: 'Cameras',
+            onClick: () => navigate('/cameras')
+        },
+        {
+            key: '3',
+            icon: <NodeIndexOutlined />,
+            label: 'Nodes',
+        },
+    ]
+
+    return (
+        <Layout>
+            <Sider trigger={null} collapsible collapsed={ collapsed } onMouseOver={ () => setCollapsed(false) } onMouseLeave={ () => setCollapsed(true) }>
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={menuOptions}/>
+            </Sider>
+            <Layout className="site-layout">
+                <Content style={ {background: colorBgContainer} }>
+                    <CamerAIRoutes/>
+                </Content>
+            </Layout>
+        </Layout>
+    )
 }
 
 export default App
