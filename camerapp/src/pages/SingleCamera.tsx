f7e820de7af06a022ab4d615095275848e5e9711
Santiago Pardal <santiagopardal@outlook.com>
2023-01-22 17:17:09 -0300

    create custom hook and clean code

--- camerapp/src/pages/SingleCamera.tsx
+++ camerapp/src/pages/SingleCamera.tsx
@@ -1,19 +1,14 @@
-import React, {useState, useEffect} from 'react'
-import Camera, {CameraJSON} from '../models/Camera'
+import React from 'react'
 import {useLocation, useParams} from 'react-router-dom'
-import {getCamera} from '../api/Cameras'
-import {useNavigate} from 'react-router-dom'
+import useCameraStatus from '../hooks/useCameraStatus'
 import {getSnapshotUrl} from '../api/Cameras'
 import {Switch} from 'antd'
 import CameraVideos from '../components/singlecamera/CameraVideos'
 
 function SingleCamera() {
-    const [camera, setCamera] = useState<Camera>()
-    const [isRecording, setIsRecording] = useState<boolean>()
     let { id } = useParams()
     let location = useLocation()
-    let navigate = useNavigate()
-
+    const [setIsRecording, camera, isRecording] = useCameraStatus(parseInt(id || '1'), location.state.camera)
     const switchRecording = () => {
         if (camera) {
             camera.record(!isRecording);
@@ -21,46 +16,15 @@ function SingleCamera() {
         }
     }
 
-    const updateIsRecording = (camera: Camera) => {
-        let recordingPromise = camera.isRecording()
-        recordingPromise
-            .then(recording => setIsRecording(recording))
-            .catch(() => navigate('/error'))
-    }
-
-    const updateCamera = async (camera?: Camera) => {
-        try {
-            let cam = camera ? camera : await getCamera(id)
-            setCamera(cam)
-            updateIsRecording(cam)
-            return cam
-        } catch (error) {
-            navigate('/error')
-        }
-    }
-
-    useEffect(() => {
-        let newCamera
-        if (location && location.state && location.state.camera) {
-            let cameraJson: CameraJSON = location.state.camera
-            newCamera = new Camera(cameraJson)
-        }
-        updateCamera(newCamera)
-    }, [])
-
-    return (
+    return camera && (
         <>
-        {camera &&
-            <>
-                <h1>{ camera.getName() }</h1>
-                <img src={ getSnapshotUrl(camera) }/>
-                <div>
-                    <span>Recording</span>
-                    <Switch checked={ isRecording } onChange={ switchRecording }></Switch>
-                </div>
-                <CameraVideos camera={camera}/>
-            </>
-        }
+            <h1>{ camera.getName() }</h1>
+            <img src={ getSnapshotUrl(camera) }/>
+            <div>
+                <span>Recording</span>
+                <Switch checked={ isRecording } onChange={ switchRecording }></Switch>
+            </div>
+            <CameraVideos camera={camera}/>
         </>
     )
 }
