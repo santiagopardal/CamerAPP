7eec1a3e062fb8253bbd495f41b54f5cec7f4136
Santiago Pardal <santiagopardal@outlook.com>
2023-01-22 16:29:10 -0300

    bring back old navbar

--- camerapp/src/App.tsx
+++ camerapp/src/App.tsx
@@ -1,52 +1,19 @@
-import React, {useState} from 'react'
+import React from 'react'
+import NavBar from './CamerAINavBar'
 import CamerAIRoutes from './Routes'
-import {NodeIndexOutlined, HomeOutlined, VideoCameraOutlined,} from '@ant-design/icons'
-import { Layout, Menu, theme } from 'antd'
-import './index.css'
-import {ItemType} from "antd/es/menu/hooks/useItems";
-import {useNavigate} from "react-router-dom";
-
-const { Sider, Content } = Layout;
+import {Layout} from 'antd'
+import {Content} from 'antd/es/layout/layout'
 
 function App() {
-    const [collapsed, setCollapsed] = useState(false);
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken()
-
-    const navigate = useNavigate()
-
-    const menuOptions: ItemType[] = [
-        {
-            key: '1',
-            icon: <HomeOutlined />,
-            label: 'Home',
-            onClick: () => navigate('')
-        },
-        {
-            key: '2',
-            icon: <VideoCameraOutlined />,
-            label: 'Cameras',
-            onClick: () => navigate('/cameras')
-        },
-        {
-            key: '3',
-            icon: <NodeIndexOutlined />,
-            label: 'Nodes',
-        },
-    ]
-
     return (
-        <Layout>
-            <Sider trigger={null} collapsible collapsed={ collapsed } onMouseOver={ () => setCollapsed(false) } onMouseLeave={ () => setCollapsed(true) }>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={menuOptions}/>
-            </Sider>
+        <>
+            <NavBar/>
             <Layout className="site-layout">
-                <Content style={ {background: colorBgContainer} }>
+                <Content>
                     <CamerAIRoutes/>
                 </Content>
             </Layout>
-        </Layout>
+        </>
     )
 }
 
--- camerapp/src/CamerAINavBar.tsx
+++ camerapp/src/CamerAINavBar.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import {Link} from 'react-router-dom'
 
 function CamerAINavBar() {
     return (
         <>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark" sticky='top'>
                 <Container>
-                    <Navbar.Brand href="/">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="/cameras">Cameras</Nav.Link>
+                    <Navbar.Brand>
+                        <Link to='/'>
+                            Navbar
+                        </Link>
+                    </Navbar.Brand>
+                    <Nav className='me-auto hover-underline-animation'>
+                        <Link to='/cameras'>
+                            Cameras
+                        </Link>
                     </Nav>
                 </Container>
             </Navbar>
