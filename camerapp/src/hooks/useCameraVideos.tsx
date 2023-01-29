1823fbc6a3dc6978118c2e7ad5b1bfbff340fbac
Santiago Pardal <santiagopardal@outlook.com>
2023-01-28 21:06:32 -0300

    cleaner code

--- camerapp/src/hooks/useCameraVideos.tsx
+++ camerapp/src/hooks/useCameraVideos.tsx
@@ -3,6 +3,7 @@ import {getVideos, Video} from '../api/Videos'
 import moment from 'moment/moment'
 import Camera from '../models/Camera'
 import useSortAndFilterPaginatableItems from './useSortAndFilterPaginatableItems'
+import {useNavigate} from 'react-router-dom'
 
 const comparator = (first: Video, second: Video) => moment(first.day, 'DD/MM/YYYY') < moment(second.day, 'DD/MM/YYYY') ? -1 : 1
 const filterFunction = () => true
@@ -10,28 +11,35 @@ const filterFunction = () => true
 const useCameraVideos = (camera: Camera, dates: string[], index: number, paginationSize: number): [Video[], number] => {
     const [videos, setVideos] = useState<Video[]>([])
     const [videosToDisplay, numberOfPages] = useSortAndFilterPaginatableItems(videos, comparator, filterFunction, index, paginationSize)
+    const navigate = useNavigate()
 
-        useEffect(() => {
-            let startDate = undefined
-            let endDate = undefined
+    const sortAndSetVideos = async (camera: Camera, startDate?: string, endDate?: string) => {
+        try {
+             let videos = await getVideos(camera, startDate, endDate)
+            videos = videos.sort(
+                (first, second) =>
+                    moment(first.day, 'DD/MM/YYYY') < moment(second.day, 'DD/MM/YYYY') ? -1 : 1
+            )
+            setVideos(videos)
+        } catch (error) {
+            navigate('/error')
+        }
+    }
 
-            if (dates.length > 0 && dates[0] !== '' && dates[1] !== '') {
-                let datesToTransform = dates.map(
-                    date => moment(date, 'DD/MM/YYYY').format('DD-MM-YYYY').toString()
-                )
-                startDate = datesToTransform[0]
-                endDate = datesToTransform[1]
-            }
+    useEffect(() => {
+        let startDate = undefined
+        let endDate = undefined
 
-            let videos = getVideos(camera, startDate, endDate)
-            videos
-                .then(
-                    videos => videos.sort(
-                        (first, second) =>
-                            moment(first.day, 'DD/MM/YYYY') < moment(second.day, 'DD/MM/YYYY') ? -1 : 1)
-                )
-                .then(setVideos)
-        }, [dates])
+        if (dates.length > 0 && dates[0] !== '' && dates[1] !== '') {
+            let datesToTransform = dates.map(
+                date => moment(date, 'DD/MM/YYYY').format('DD-MM-YYYY').toString()
+            )
+            startDate = datesToTransform[0]
+            endDate = datesToTransform[1]
+        }
+
+        sortAndSetVideos(camera, startDate, endDate)
+    }, [dates])
 
     return [videosToDisplay, numberOfPages]
 }
